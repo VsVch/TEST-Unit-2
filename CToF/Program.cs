cea3f2d77bbec7f0f8afaf864f6dc22f9d1a2238
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-10-07 19:23:05 +0300

    Add project files.

---
+++ CToF/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CToF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+            double cap = l * h * d;
+            Double tl = cap * 0.001;
+            double cp = p * .01;
+            double lN = tl * (1-cp);
+            Console.WriteLine("{0:f3}",lN);
+        }
+    }
+}
---
+++ Cyrcle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cyrcle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Double area = a*b/2;
+            Console.WriteLine("{0:f2}", area);
+           
+           
+        }
+    }
+}
